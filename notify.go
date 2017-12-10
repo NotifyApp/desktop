e3c559d9bf9e4046b98e4805e9be23b49a28fe54
ItsJimi <jimi.maiquez@gmail.com>
2017-12-10 01:10:50 +0100

    Fix incoming notifs

--- notify.go
+++ notify.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-	"runtime"
-	"fmt"
-	"os/exec"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
 
 	"github.com/gorilla/websocket"
 )
 
 func notify(text string, title string, sound string, icon string) {
 	switch runtime.GOOS {
-		case "darwin":
-			notification := fmt.Sprintf("display notification \"%s\" with title \"%s\" sound name \"%s\"", text, title, sound)
-			cmd := exec.Command("osascript", "-e", notification)
-			cmd.Start()
-		case "linux":
-			cmd := exec.Command("notify-send", "-i", icon, title, text)
-			cmd.Start()
-		case "windows":
-			fmt.Printf("Soon\n")
+	case "darwin":
+		notification := fmt.Sprintf("display notification \"%s\" with title \"%s\" sound name \"%s\"", text, title, sound)
+		cmd := exec.Command("osascript", "-e", notification)
+		cmd.Start()
+	case "linux":
+		cmd := exec.Command("notify-send", "-i", icon, title, text)
+		cmd.Start()
+	case "windows":
+		fmt.Printf("Soon\n")
 	}
 }
 
 type Notification struct {
-	Message string
+	Title   string `json:"title"`
+	Message string `json:"message"`
 }
 
 func main() {
 	fmt.Printf("Start\n")
 
-	url := "ws://localhost:5000/"
+	url := "ws://localhost:8080/ws"
 	var dialer *websocket.Dialer
 
 	conn, _, err := dialer.Dial(url, nil)
@@ -47,11 +48,10 @@ func main() {
 			return
 		}
 
-		res := Notification{}
-
+		var res Notification
 		json.Unmarshal(msg, &res)
 
-		notify(res.Message, "test", "", "")
+		notify(res.Message, res.Title, "", "")
 
 		log.Printf("received: %s\n", msg)
 	}
